544a23a10c3acfb3b5fad252a9ba2f452fa33b69
Douglas Obara <douglasobara97@gmail.com>
2022-06-13 18:20:33 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let li = document.createElement("li");
+  li.textContent = `${todo} `;
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "X";
+  li.appendChild(btn);
+
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
